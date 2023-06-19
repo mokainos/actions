96bfca99d5b098cd5e5634cb99c61afd425b46f0
mokainos <72137340+mokainos@users.noreply.github.com>
2023-06-19 23:31:58 +0100

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -1,1 +1,19 @@
 
+import json
+import os
+
+listObj = []
+filepath = 'issues_list.json'
+new_data = os.environ["NEW_DATA"]
+try:
+  with open(filepath, "r") as json_file:
+    listObj = json.load(json_file)
+except FileNotFoundError:
+  with open(filepath, "w") as json_file:
+    listObj.append(new_data)
+    json.dump(listObj, json_file, indent=4)
+else:
+  listObj.append(new_data)
+  with open(filepath, "w") as json_file:
+    json.dump(listObj, json_file, indent=4)
+
